5d0c381a3f52e99dfaf979e279e7b4ea27478a49
Lucas Lira <lucasliradev@gmail.com>
2024-09-09 10:55:48 -0300

    todo_list - todos e reminders

--- todo_list/src/index.ts
+++ todo_list/src/index.ts
@@ -1,1 +1,40 @@
-alert('Funcionou!');
+(() => {
+    const todo = {
+        description: 'todo',
+        done: false
+    }
+
+    const reminder = {
+        description: 'reminder',
+        date: '2024-09-09'
+    }
+
+    const taskView ={
+        render(tasks: Array<object>){
+            const tasksList = document.getElementById('tasksList');
+
+            while(tasksList?.firstChild){
+                tasksList.removeChild(tasksList.firstChild);
+            }
+            tasks.forEach( task => {
+                const li = document.createElement('LI');
+                const textNode = document.createTextNode(JSON.stringify(task));
+                li.appendChild(textNode);
+                tasksList?.appendChild(li);
+            })
+        }
+    }
+
+    const TaskController = (view: typeof taskView) => {
+        const tasks: Array<object> = [todo, reminder];
+
+        const handleEvent = (event: Event) => {
+            event.preventDefault();
+            view.render(tasks);
+        }
+
+        document.getElementById('taskForm')?.addEventListener('submit', handleEvent);
+    }
+
+    TaskController(taskView);
+})();
